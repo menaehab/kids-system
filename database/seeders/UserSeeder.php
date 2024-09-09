cc24fa5a97317b11c3a6cd4f918302cfc654f3f9
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-09-10 02:16:57 +0300

    Make admin account seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(['email' => 'admin@gmail.com'], [
+            'name' => 'admin',
+            'password' =>  bcrypt('menamena'),
+        ]);
+    }
+}
